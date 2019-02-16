e7a80a26c0e1257aeb1d3568eba5025f185918af
yoon.homme <woosiks.io@gmail.com>
2019-02-16 18:43:02 +0900

    Add gcloud settings

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const yaml = require("js-yaml");
 const fs = require("fs");
-var Autolinker = require("autolinker");
+const Autolinker = require("autolinker");
 
 const app = express();
 const port = process.env.PORT || 3000;
